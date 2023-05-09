3f81f6eec83c3dabdfd1d8ae0a3225584832e7a0
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-05-09 13:08:44 +0530

    Added : signup call successfully

---
+++ src/main/java/com/indiatour/etour/OpticalLoginSignup.java
@@ -0,0 +1,15 @@
+package com.indiatour.etour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticalLoginSignup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpticalLoginSignup.class, args);
+	}
+
+	
+}
+
---
+++ src/main/java/com/indiatour/etour/model/ERole.java
@@ -0,0 +1,8 @@
+package com.indiatour.etour.model;
+
+public enum ERole {
+
+		  ROLE_USER,
+		  ROLE_ADMIN
+
+}
